943390ffa5cfd8e3ec187a1d2117756d209b96da
Alex Nagy <alexnagy@spacefiller.space>
2022-01-13 22:32:54 -0800

    finished flow field

--- sketch.js
+++ sketch.js
@@ -5,21 +5,37 @@
 //https://www.youtube.com/watch?v=BjoM9oKOAKY
 
 
+const NUM_PARTICLES = 5000;
+const MAG = .05;
+
 let inc = 0.1;
 let scl = 10;
 let zOff = 0;
 let zInc = 0.01;
 
-let cols, rows;
 
+
+
+let cols, rows;
 let fr;
 
+var particles = [];
+
+var flowField = [];
+
+
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   pixelDensity(1);
   cols = floor(width / scl);
   rows = floor(height / scl);
   fr = createP('');
+
+  flowField = new Array(cols * rows);
+
+  for (var i = 0; i < NUM_PARTICLES; i ++){ 
+    particles[i] = new Particle();
+  }
 }
 
 function draw() {
@@ -30,21 +46,32 @@ function draw() {
   for(let y = 0; y < rows; y ++){
     let xOff = 0;
     for(let x = 0; x < cols; x ++){
-      let index = (x + y * width) * 4;
+      let index = (x + y * cols);
       let angle = noise(xOff, yOff, zOff)* TAU;
       let v = p5.Vector.fromAngle(angle);
+      v.setMag(MAG);
+      flowField[index] = v;
       xOff += inc;
-      stroke(0);
+      stroke(0, 100);
       push();
       translate(x * scl, y * scl);
       rotate(v.heading());
+      strokeWeight(1);
       line(0, 0, scl, 0);
 
       pop();
     }
     yOff+= inc;
     
-    fr.html(floor(frameRate()));
   }
+  for (var i = 0; i < NUM_PARTICLES; i ++){ 
+    particles[i].update();
+    particles[i].show();
+    particles[i].edges();
+    particles[i].follow(flowField);
+
+  }
+  
+  fr.html(floor(frameRate()));
   zOff += zInc;
 }
