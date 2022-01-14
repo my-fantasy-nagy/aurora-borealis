c209a0a36bc5d0eaa6b62c92360079ecd4dd35fc
Alex Nagy <alexnagy@spacefiller.space>
2022-01-13 21:00:30 -0800

    First Commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+//Aurora borealis like sketch using 3D noise and flow fields
+
+
+//Inspired by Daniel Schifmann's Perlin Noise Flow Field Example
+//https://www.youtube.com/watch?v=BjoM9oKOAKY
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
