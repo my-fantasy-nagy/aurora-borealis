5c96ed648459389ba461e3a490d871dc13af4549
Alex Nagy <alexnagy@spacefiller.space>
2022-01-13 21:30:36 -0800

    Perlin 2D noise example with pixels

--- sketch.js
+++ sketch.js
@@ -5,10 +5,35 @@
 //https://www.youtube.com/watch?v=BjoM9oKOAKY
 
 
+var inc = 0.01;
+
+
 function setup() {
   createCanvas(400, 400);
+  pixelDensity(1);
+  
 }
 
 function draw() {
-  background(220);
+  var yOff = 0.0;
+  loadPixels();
+
+  
+  for(var x = 0; x < width; x ++){
+    var xOff = 0.0;
+    for(var y = 0; y < height; y ++){
+      var index = (x + y * width) * 4;
+      var r = noise(xOff, yOff) * 255;
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+
+      xOff += inc;
+    }
+    yOff+= inc;
+    
+  }
+  updatePixels();
+  noLoop();
 }
