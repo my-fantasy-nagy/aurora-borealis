51d5c5f24e9251a71c6c799906870c795c4de96a
Alex Nagy <alexnagy@spacefiller.space>
2022-01-16 19:57:31 -0800

    test

--- sketch.js
+++ sketch.js
@@ -26,27 +26,24 @@ function draw() {
   background(255);
   let yOff = 0.0;
   
-  let count = 0;
   for(let y = 0; y < rows; y ++){
     let xOff = 0;
     for(let x = 0; x < cols; x ++){
       let index = (x + y * width) * 4;
       let magnitude = noise(xOff, yOff, zOff) - 0.5;
-      
       let v = p5.Vector.fromAngle(-PI/2);
       xOff += inc;
       stroke(0);
+
       push();
       translate(x * scl, y * scl);
       rotate(v.heading());
       line(0, 0, 5, 0);
-
       pop();
-      count ++;
     }
     yOff+= inc;
   }
-  print(count);
+
   noLoop();
   fr.html(floor(frameRate()));
   // zOff += zInc;
